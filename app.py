0e7b86ed9af69e03fe5e23a8d8ee4780e0dc52af
riyaroy2086 <89868832+riyaroy2086@users.noreply.github.com>
2022-11-29 20:16:16 +0530

    deployment changes

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model = pickle.load(open('C:/Users/DELL/OneDrive/Desktop/diabetes test ml app/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 def diabetes_prediction(input_data):
 
